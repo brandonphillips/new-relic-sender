fed2623772117a0187bf0124729ed6e3c40dc849
brandonphillips <proecl@gmail.com>
2020-03-17 19:28:31 -0500

    Testing input

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"os"
+	"fmt"
 )
 
+type Log struct {
+	eventType	string `json:"eventType"`
+	message		string `json:"message"`
+}
+
 func main() {
-	app := setupCli()
-	app.Run(os.Args)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter eventType: ")
+	inputEventType, _ := reader.ReadString('\n')
+	fmt.Print("Enter message: ")
+	inputMessage, _ := reader.ReadString('\n')
+
+	var currentLog Log;
+	currentLog.eventType = inputEventType
+	currentLog.message = inputMessage
+	
+	fmt.Print("Log: ", currentLog);
 }
